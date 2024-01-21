25c7565fbf2532d9325a7458ba4362a686022727
Manoj882 <manojbk488@gmail.com>
2024-01-21 09:53:26 +0545

    modified checkIfPangram

--- HashMap/CheckIfPanagram.java
+++ HashMap/CheckIfPanagram.java
@@ -1,33 +1,63 @@
 import java.util.*;
 
+// public class CheckIfPanagram {
+
+//     public static boolean checkIfPanagram(String str){
+
+//         Set<Character> set = new HashSet<Character>();
+
+//         for(int i =0; i < str.length(); i++){
+//             char c = str.toLowerCase().charAt(i);
+
+//             if(c >= 'a' && c <='z'){
+//                 set.add(c);
+//             }
+//         }
+
+
+//         return set.size() == 26;
+//     }
+
+
+
+//     public static void main(String[] args) {
+
+//         String input = "123thequickbrownfoxjumpsoverthelazydog";
+
+//         boolean result = checkIfPanagram(input);
+
+//         System.out.println(result);
+        
+//     }
+    
+// }
+
+
 public class CheckIfPanagram {
 
-    public static boolean checkIfPanagram(String str){
+    public static boolean checkIfPangram(String sentence){
 
         Set<Character> set = new HashSet<Character>();
+        int length = sentence.length();
 
-        for(int i =0; i < str.length(); i++){
-            char c = str.toLowerCase().charAt(i);
+        for(int i =0; i < length; i++){
+            char c = sentence.toLowerCase().charAt(i);
 
-            if(c >= 'a' && c <='z'){
+            if(!set.contains(c) && (c >='a' && (c <='z'))){
                 set.add(c);
             }
         }
 
 
+
         return set.size() == 26;
     }
 
-
-
     public static void main(String[] args) {
 
-        String input = "thequickbrownfoxjumpsoverthelazydog";
-
-        boolean result = checkIfPanagram(input);
-
-        System.out.println(result);
+        String sentence = "123thequickbrownfoxjumpsoverthelazydog";
+        boolean output = checkIfPangram(sentence);
+        System.out.println(output);
         
     }
-    
 }
