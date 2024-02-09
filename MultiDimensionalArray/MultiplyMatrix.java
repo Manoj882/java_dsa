b4f09fa80a6be0bab8dc8d7adf5cdc3a47a84f20
Manoj882 <manojbk488@gmail.com>
2024-02-09 08:46:58 +0545

    multiply matrix

---
+++ MultiDimensionalArray/MultiplyMatrix.java
@@ -0,0 +1,80 @@
+
+/*
+    Write a program to display Multiplication of two matrices entered by
+    the user.
+ */
+
+import java.util.Scanner;
+
+public class MultiplyMatrix {
+
+    static void printMatrix(int[][] matrix){
+        for(int i =0; i < matrix.length; i++){
+            for(int j =0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    } 
+
+    static void multiplyMatrix(int[][] arr1, int r1, int c1, int[][] arr2, int r2, int c2){
+        if(c1 != r2){
+            System.out.println("Wrong input - Multiplication not possible");
+        }
+
+        int[][] mul = new int[r1][c2];
+
+        for(int i =0; i < r1; i++){ // row number
+            for(int j =0; j <c2; j++){ // column number
+                for(int k =0; k < c1; k++){
+                    mul[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        System.out.println("Multiplication of two matrices: ");
+        printMatrix(mul);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows and columns of matrix 1: ");
+
+        int r1 = sc.nextInt();
+        int c1 = sc.nextInt();
+        
+        int[][] arr1 = new int[r1][c1];
+
+        System.out.println("Enter matrix values: ");
+
+        for(int i =0; i < r1; i++){
+            for(int j =0; j < c1; j++){
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter number of rows and columns of matrix 1: ");
+
+        int r2 = sc.nextInt();
+        int c2 = sc.nextInt();
+        
+        int[][] arr2 = new int[r2][c2];
+
+        System.out.println("Enter matrix values: ");
+
+        for(int i =0; i < r2; i++){
+            for(int j =0; j < c2; j++){
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        multiplyMatrix(arr1, r1, c1, arr2, r2, c2);
+
+        
+    }
+
+
+    
+}
